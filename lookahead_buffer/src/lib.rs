e5bfd0051900835e72f703a163d77b9f55d1a442
Josh <joshrasmussen34@gmail.com>
2021-05-17 14:58:58 -0700

    fix(lookahead_buffer): update to use better ownership semantics
    Rather than relying on lifetimes LA takes ownership of the vector instead of borrowing (which is much more complex)

--- lookahead_buffer/src/lib.rs
+++ lookahead_buffer/src/lib.rs
@@ -1,32 +1,20 @@
-pub struct LookaheadBuffer<T> {
+pub struct LookaheadBuffer<T: Clone> {
     buf: Vec<T>,
     start: usize,
     current: usize
 }
 
-impl From<&str> for LookaheadBuffer<u8> {
-    fn from(input: &str) -> Self {
-        LookaheadBuffer::new(Vec::from(input))
-    }
-}
-
-impl<T> From<Vec<T>> for LookaheadBuffer<T> {
-    fn from(input: Vec<T>) -> Self {
-        LookaheadBuffer::new(input)
-    }
-}
-
-impl<T> LookaheadBuffer<T> {
+impl<T: Clone> LookaheadBuffer<T> {
     pub fn new(buffer: Vec<T>) -> Self {
         LookaheadBuffer { buf: buffer, current: 0, start: 0 }
     }
 
-    pub fn peek(&self, n: usize) -> Option<&T> {
+    pub fn peek(&self, n: usize) -> Option<T> {
         if self.current + n >= self.buf.len() {
             return None;
         }
 
-        Some(&self.buf[self.current + n])
+        Some(self.buf[self.current + n].clone())
     }
 
     pub fn advance(&mut self) {
@@ -37,8 +25,8 @@ impl<T> LookaheadBuffer<T> {
         self.current = self.current + 1;
     }
 
-    pub fn get_slice(&self) -> &[T] {
-        &self.buf[self.start..self.current]
+    pub fn get_slice(&self) -> Vec<T> {
+        self.buf[self.start..self.current].to_vec()
     }
 
     pub fn commit(&mut self) {
--- lookahead_buffer/tests/integration_tests.rs
+++ lookahead_buffer/tests/integration_tests.rs
@@ -6,14 +6,16 @@ mod tests {
 
     #[test]
     fn test_can_peek_at_top_values() {
-        let input: LookaheadBuffer<u8> = "abcd".into();
-        assert_eq!(&b'a', input.peek(0).unwrap());
-        assert_eq!(&b'b', input.peek(1).unwrap());
+        let input = Vec::from("abcd");
+        let input: LookaheadBuffer<u8> = LookaheadBuffer::new(input);
+        assert_eq!(b'a', input.peek(0).unwrap());
+        assert_eq!(b'b', input.peek(1).unwrap());
     }
 
     #[test]
     fn test_can_consume_slice() {
-        let mut input: LookaheadBuffer<u8> = "abcd".into();
+        let input = Vec::from("abcd");
+        let mut input: LookaheadBuffer<u8> = LookaheadBuffer::new(input);
         input.advance();
         input.advance();
         input.advance();
@@ -22,51 +24,54 @@ mod tests {
         let slice_b = input.get_slice();
         let top = input.peek(0);
 
-        assert_eq!(&[b'a', b'b', b'c'], slice_a);
-        assert_eq!(&[b'a', b'b', b'c'], slice_b);
-        assert_eq!(&b'd', top.unwrap());
+        assert_eq!(vec![b'a', b'b', b'c'], slice_a);
+        assert_eq!(vec![b'a', b'b', b'c'], slice_b);
+        assert_eq!(b'd', top.unwrap());
     }
 
     #[test]
     fn test_can_consume_different_slices_after_commit() {
-        let mut input: LookaheadBuffer<u8> = "abcd".into();
+        let input = Vec::from("abcd");
+        let mut input: LookaheadBuffer<u8> = LookaheadBuffer::new(input);
         input.advance();
         input.advance();
         input.advance();
 
         let slice_a = input.get_slice();
-        assert_eq!(&[b'a', b'b', b'c'], slice_a);
+        assert_eq!(vec![b'a', b'b', b'c'], slice_a);
 
         input.commit();
         input.advance();
 
         let slice_b = input.get_slice();
-        assert_eq!(&[b'd'], slice_b);
+        assert_eq!(vec![b'd'], slice_b);
     }
 
     #[test]
     fn test_can_consume_empty_slices_after_commit() {
-        let mut input: LookaheadBuffer<u8> = "abcd".into();
+        let input = Vec::from("abcd");
+        let mut input: LookaheadBuffer<u8> = LookaheadBuffer::new(input);
         input.advance();
         input.advance();
         input.advance();
 
         let slice_a = input.get_slice();
-        assert_eq!(&[b'a', b'b', b'c'], slice_a);
+        assert_eq!(vec![b'a', b'b', b'c'], slice_a);
 
         input.commit();
 
-        let slice_b: &[u8] = input.get_slice();
-        let expected: &[u8] = &[];
+        let slice_b: Vec<u8> = input.get_slice();
+        let expected: Vec<u8> = vec![];
         assert_eq!(expected, slice_b);
 
         let top = input.peek(0);
-        assert_eq!(&b'd', top.unwrap());
+        assert_eq!(b'd', top.unwrap());
     }
 
     #[test]
     fn test_peek_returns_null_byte_when_at_end() {
-        let mut input: LookaheadBuffer<u8> = "abcd".into();
+        let input = Vec::from("abcd");
+        let mut input: LookaheadBuffer<u8> = LookaheadBuffer::new(input);
         input.advance();
         input.advance();
         input.advance();
@@ -79,7 +84,8 @@ mod tests {
 
     #[test]
     fn test_advancing_past_length_of_buffer_does_nothing() {
-        let mut input: LookaheadBuffer<u8> = "abcd".into();
+        let input = Vec::from("abcd");
+        let mut input: LookaheadBuffer<u8> = LookaheadBuffer::new(input);
         input.advance();
         input.advance();
         input.advance();
@@ -88,7 +94,7 @@ mod tests {
         input.advance();
 
         let slice = input.get_slice();
-        assert_eq!(&[b'a', b'b', b'c', b'd'], slice);
+        assert_eq!(vec![b'a', b'b', b'c', b'd'], slice);
 
         input.commit();
 
